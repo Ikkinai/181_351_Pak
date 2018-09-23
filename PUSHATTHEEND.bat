b8a0c05a02ad86852f808023bc0bdad20ba4579e
Ikkinai <sumtrec@ya.ru>
2018-09-23 18:36:32 +0300

    23.09.2018_18:36:32,56_Pak_Natasha_181_351_polytech
    Signed-off-by: Ikkinai <sumtrec@ya.ru>

--- PUSHATTHEEND.bat
+++ PUSHATTHEEND.bat
@@ -5,6 +5,6 @@ git add *.sln
 git add *.vcxproj 
 git add *.user
 git add *.bat
-git commit -sm "%DATE% %TIME%" 
+git commit -sm "%DATE%_%TIME%_Pak_Natasha_181_351_polytech" 
 git push iamiki master
 pause
